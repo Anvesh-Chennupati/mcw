2160db73374fcbd4c7872467e5510cce08dfdc7f
Anvesh <anveshchowdary01@gmail.com>
2019-06-24 20:26:49 -0700

    added functions

--- matrix_Multiplication/header.h
+++ matrix_Multiplication/header.h
@@ -2,6 +2,7 @@
 #ifdef header.h
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 void printMatrix(int** Matrix, unsigned int row, unsigned int column);
 int** multiply1(int** Matrix1, int** Matrix2,unsigned int row, unsigned int column);
---
+++ matrix_Multiplication/operations.c
@@ -0,0 +1,26 @@
+#include"header.h"
+
+void printMatrix(int** Matrix, unsigned int row, unsigned int column){
+    int i,j;
+    for(i =0; i< row; i++){
+        for(j =0; j<column; j++){
+            printf("%d ",Matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+int** multiply1(int** Matrix1, int** Matrix2,unsigned int row, unsigned int column){
+    int i, j,k;
+    int** Matrix = malloc(row * sizeof(int));
+    for(i =0; i<row; i++)
+        Matrix[i] = malloc(column * sizeof(int));
+    for(i =0; i< row; i++){
+        for(j =0; j< column; j++){
+            Matrix[i][j] = 0;
+            for(k =0; k< row; k++){
+                Matrix[i][j] += Matrix1[i][k] * Matrix2[k][j]; 
+            }
+        }
+    }
+    return Matrix;
+}
