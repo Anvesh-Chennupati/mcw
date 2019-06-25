873d1613fd19dfba6f2c9b60038735114d1ca5ea
Anvesh <anveshchowdary01@gmail.com>
2019-06-24 20:20:26 -0700

    #ifdef

--- matrix_Multiplication/header.h
+++ matrix_Multiplication/header.h
@@ -1,7 +1,9 @@
 #pragma once
-
+#ifdef header.h
 #include<stdio.h>
 #include<stdlib.h>
 
 void printMatrix(int** Matrix, unsigned int row, unsigned int column);
 int** multiply1(int** Matrix1, int** Matrix2,unsigned int row, unsigned int column);
+
+#endif
