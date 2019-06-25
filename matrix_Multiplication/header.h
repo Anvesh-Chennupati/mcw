554c55007b3879167eb0af461e2fd2d8f06820a2
Anvesh <anveshchowdary01@gmail.com>
2019-06-24 20:19:34 -0700

    initial commit

---
+++ matrix_Multiplication/header.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void printMatrix(int** Matrix, unsigned int row, unsigned int column);
+int** multiply1(int** Matrix1, int** Matrix2,unsigned int row, unsigned int column);
